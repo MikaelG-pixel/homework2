dd591c89bc5a27726cb11628d4a4c3f6988095fb
MikaelG-pixel <ork999sss@mail.ru>
2024-07-26 14:27:29 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_of_homework = '12'
+number_of_hours = '1.5'
+course_name = 'Python'
+time_for_one_task = '0.125'
+print('Курс: '+course_name+', всего задач: '+number_of_homework+', затрачено часов: '+number_of_hours+', среднее время выполнения '+time_for_one_task+' часа.')
