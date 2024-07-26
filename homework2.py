b28b296c425039a96dc6537f0c7efcc9987810b4
MikaelG-pixel <ork999sss@mail.ru>
2024-07-26 18:29:17 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,5 +1,7 @@
 number_of_homework = '12'
+a = int(number_of_homework)
 number_of_hours = '1.5'
+b = float(number_of_hours)
 course_name = 'Python'
-time_for_one_task = '0.125'
+time_for_one_task = str(b/a)
 print('Курс: '+course_name+', всего задач: '+number_of_homework+', затрачено часов: '+number_of_hours+', среднее время выполнения '+time_for_one_task+' часа.')
